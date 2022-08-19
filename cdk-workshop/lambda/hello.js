4d5db17efd2493a7a94845442ffd17e6da1ecf96
Ben-Withnell <ben.withnell@york.ac.uk>
2022-08-19 09:17:39 +0000

    Added new files and completed stages of the CDK workshop

---
+++ cdk-workshop/lambda/hello.js
@@ -0,0 +1,8 @@
+exports.handler = async function(event) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello Ben you are cool, CDK! You've hit ${event.path}\n`
+  };
+};
